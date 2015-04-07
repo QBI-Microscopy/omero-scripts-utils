983bf43b6c8f7086d6958e135fd8480893961453
Daniel Matthews <dr.dan.matthews@gmail.com>
2015-04-07 16:21:58 +1000

    for 5.0.8 do not get values on physicalpixel size

--- OMEExporter/ome_metadata.py
+++ OMEExporter/ome_metadata.py
@@ -58,9 +58,9 @@ class OMEExporter(OMEBase):
                 self.ROI = ROI
                 self.roi_count = len(ROI)
                 
-            self.Xres = source.getPrimaryPixels().getPhysicalSizeX().getValue()
-            self.Yres = source.getPrimaryPixels().getPhysicalSizeY().getValue()
-            self.Zres = source.getPrimaryPixels().getPhysicalSizeZ().getValue()
+            self.Xres = source.getPrimaryPixels().getPhysicalSizeX()
+            self.Yres = source.getPrimaryPixels().getPhysicalSizeY()
+            self.Zres = source.getPrimaryPixels().getPhysicalSizeZ()
             print 'xres,yres,zres:',self.Xres,self.Yres,self.Zres
             self.dtype = source.getPixelsType()
             print 'dtype',type(self.dtype)
